81ff37a80cbabaa083ab491aafcd0d9c9ba9b1a4
tkwbr999 <tkwbr999@gmail.com>
2025-04-03 00:31:47 +0900

    pages用の設定ファイル変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
-import path from "path" // path をインポート
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { componentTagger } from 'lovable-tagger';
+import path from 'path'; // path をインポート
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: { // resolve を追加
-    alias: {
-      "@": path.resolve(__dirname, "./src"), // @ エイリアスを設定
+export default defineConfig(({ mode }) => {
+  // GitHub Pages用のベースパスを設定
+  // HashRouterを使用する場合、ベースURLは単純な相対パスでよい
+  const base = process.env.GITHUB_PAGES ? '/go-cheat-sheet-garden/' : './';
+
+  return {
+    base,
+    define: {
+      'import.meta.env.GITHUB_PAGES': JSON.stringify(process.env.GITHUB_PAGES),
     },
-  },
-})
+    server: {
+      host: '::',
+      port: 8080,
+    },
+    plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
+});
